63102905a196bb45ae001fcd2183e52086c16e86
thodoxuan99 <tho.dok2017@hcmut.edu.vn>
2021-07-14 11:08:48 +0700

    re-factor install

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
-docker-compose up -d
+git clone https://github.com/thodoxuan99/EnvironmentMonitor--ELK_RabbitMQ.git
+cd EnvironmentMonitor--ELK_RabbitMQ
+git checkout re_config
+docker-compose up --force-recreate  -d
 #Config SSL for MQTT
 docker cp rabbitmq.conf RabbitMQ:/etc/rabbitmq/
 docker cp ssl RabbitMQ:/
