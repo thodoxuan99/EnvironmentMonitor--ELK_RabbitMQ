578ce6c01572749129a38ce55dea5d356b85783a
thodoxuan99 <tho.dok2017@hcmut.edu.vn>
2021-07-14 16:53:45 +0700

    re-factor install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/thodoxuan99/EnvironmentMonitor--ELK_RabbitMQ.git
 cd EnvironmentMonitor--ELK_RabbitMQ
 git checkout re_config
+docker rm RabbitMQ
 docker-compose up --force-recreate  -d
 docker restart ELK_RabbitMQ
 docker exec -it RabbitMQ rabbitmq-plugins enable rabbitmq_mqtt
