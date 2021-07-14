76742c50fe69cfb14fa9717fb3f52cff189d9f5b
thodoxuan99 <tho.dok2017@hcmut.edu.vn>
2021-07-14 17:12:43 +0700

    add commit

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 git clone https://github.com/thodoxuan99/EnvironmentMonitor--ELK_RabbitMQ.git
 cd EnvironmentMonitor--ELK_RabbitMQ
 git checkout re_config
-docker rm  -f RabbitMQ
 docker-compose up --force-recreate  -d
-docker restart ELK_RabbitMQ
+#Wait for starting service
 docker exec -it RabbitMQ rabbitmq-plugins enable rabbitmq_mqtt
