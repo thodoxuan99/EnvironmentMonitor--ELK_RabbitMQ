6e7d61e79521febe99f423075fc2489f32c8f8f9
thodoxuan99 <tho.dok2017@hcmut.edu.vn>
2021-07-14 16:52:02 +0700

    change install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,5 @@ git clone https://github.com/thodoxuan99/EnvironmentMonitor--ELK_RabbitMQ.git
 cd EnvironmentMonitor--ELK_RabbitMQ
 git checkout re_config
 docker-compose up --force-recreate  -d
-#Config SSL for MQTT
-docker cp rabbitmq.conf RabbitMQ:/etc/rabbitmq/
-docker cp ssl RabbitMQ:/
-docker exec RabbitMQ rabbitmq-plugins enable rabbitmq_mqtt
-docker restart RabbitMQ
+docker restart ELK_RabbitMQ
+docker exec -it RabbitMQ rabbitmq-plugins enable rabbitmq_mqtt
